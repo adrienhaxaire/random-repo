e1ba6016cd2b82836df7e5959e899a866ee1f9ef
Adrien Haxaire <adrien@numerry.com>
2017-01-29 14:12:42 +0100

    Querying database instead of hard coded airports.

--- app/models/Airport.scala
+++ app/models/Airport.scala
@@ -1,18 +1,23 @@
 package models
 
+import play.api.Play.current
+import play.api.db.DB
+import anorm.SQL
+import anorm.SqlQuery
 
 case class Airport (ident: String, name: String, municipality: String, countryCode: String)
 
 object Airport {
 
-  var airports = Set(
-    Airport("FR-0001", "Centre Hospitalier De Moenchsberg Heliport", "Mulhouse", "FR"),
-    Airport("FR-0002", "Centre Hospitalier Hautepierre Heliport", "Strasbourg", "FR"),
-    Airport("FR-0003", "Centre Hospitalier Heliport", "Haguenau", "FR"),
-    Airport("FR-0004", "Centre Hospitalier Heliport", "Ingwiller", "FR")
-  )
+  def byCountryCode(code: String): List[Airport] = {
+    val sql: SqlQuery = SQL("select * from airports")
+    DB.withConnection { implicit connection =>
+      sql().map(row =>
+        Airport(row[String]("ident"),
+          row[String]("name"),
+          row[String]("municipality"),
+          row[String]("country_code"))).toList}
+  }
 
-  def byCountryCode(code: String) = airports.toList.filter(_.countryCode == code)
 }
-
 
