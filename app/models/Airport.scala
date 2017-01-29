19ee35923a84e575c75545c07265930ca1f3164f
Adrien Haxaire <adrien@numerry.com>
2017-01-29 17:44:51 +0100

    Forgot to restore the filtering of the query on code_country.
    That's why Akka crashed due to insufficient memory.

--- app/models/Airport.scala
+++ app/models/Airport.scala
@@ -10,7 +10,7 @@ case class Airport (faa: String, name: String, latitude: Double, longitude: Doub
 object Airport {
 
   def byCountryCode(code: String): List[Airport] = {
-    val sql: SqlQuery = SQL("select * from airports")
+    val sql: SqlQuery = SQL("select * from airports where country_code = '" + code + "';")
     DB.withConnection { implicit connection =>
       sql().map(row =>
         Airport(row[String]("faa"),
